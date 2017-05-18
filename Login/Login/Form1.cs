4b4d39076982fb7f6276ca944fa08fddf8ece71b
mariamghazaryan <28866455@mail.ru>
2017-05-19 00:15:11 +0400

    Add files via upload

---
+++ Login/Login/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Login
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.button1.Enabled = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (this.textBox1.Text == "Admin" && this.textBox2.Text == "Admin")
+            {
+                MessageBox.Show("Your Username and Password are correct.");
+            }
+            else
+            {
+                MessageBox.Show("Wrong input!");
+                Application.Exit();
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                this.button1.Enabled = true;
+            }
+            else
+            {
+                this.button1.Enabled = false;
+            }
+        }
+    }
+}
